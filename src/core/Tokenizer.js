de683f52a655a4276105350ed0db25bb27f6b332
Joseph Hu <joseph.hu@databricks.com>
2024-07-09 17:21:12 -0700

    add support for r string

--- src/core/Tokenizer.js
+++ src/core/Tokenizer.js
@@ -77,6 +77,7 @@ export default class Tokenizer {
             "N''": "((N'[^N'\\\\]*(?:\\\\.[^N'\\\\]*)*('|$))+)",
             "{{}}": "({{(.*?)}})",
             "${}": "(\\${(.*?)})",
+            "r''": "(r'(.*?)')",
         };
 
         return stringTypes.map(t => patterns[t]).join("|");
